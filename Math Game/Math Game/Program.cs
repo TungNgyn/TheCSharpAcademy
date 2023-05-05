9ee674d976ffc7c8118928e02a36a7b0d8d2024c
Tung Nguyen <Nguyen.Tung@outlook.de>
2023-05-05 09:17:57 +0200

    finished math game with history output and exception handling

--- Math Game/Math Game/Program.cs
+++ Math Game/Math Game/Program.cs
@@ -1,56 +1,73 @@
-﻿List<String> record;
+﻿List<string> record = new List<string>();
 
 void startGame()
 {
-
+    Console.WriteLine();
     Console.WriteLine("----- Math Game -----");
     Console.WriteLine("Write 'help' to see all commands");
     Console.Write("First number: ");
-    var firstNumber = Console.ReadLine()!;
-    if (firstNumber == "help")
-    {
-        getHelp();
-    }
+    string firstNumber = Console.ReadLine()!;
+    checkInput(firstNumber);
 
     Console.Write("Second number: ");
-    var secondNumber = Console.ReadLine()!;
-    if (secondNumber == "help")
-    {
-        getHelp();
-    }
+    string secondNumber = Console.ReadLine()!;
+    checkInput(secondNumber);
 
     Console.Write("Arithmetic operation (+,-,*,/): ");
     string operation = Console.ReadLine()!;
-    if (operation == "help")
-    {
-        getHelp();
-    }
 
     switch (operation)
     {
         case ("+"):
             Console.WriteLine("The sum is " + (int.Parse(firstNumber) + int.Parse(secondNumber)));
-            record.Add("The sum is " + (int.Parse(firstNumber) + int.Parse(secondNumber)));
+            record.Add($"The sum of {int.Parse(firstNumber)} and {int.Parse(secondNumber)} is " + (int.Parse(firstNumber) + int.Parse(secondNumber)) + ".");
             break;
         case ("-"):
             Console.WriteLine("The difference is " + (int.Parse(firstNumber) - int.Parse(secondNumber)));
-            record.Add("The sum is " + (int.Parse(firstNumber) - int.Parse(secondNumber)));
+            record.Add($"The difference of {int.Parse(firstNumber)} and {int.Parse(secondNumber)} is " + (int.Parse(firstNumber) - int.Parse(secondNumber)) + ".");
             break;
         case ("*"):
             Console.WriteLine("The product is " + (int.Parse(firstNumber) * int.Parse(secondNumber)));
-            record.Add("The sum is " + (int.Parse(firstNumber) * int.Parse(secondNumber)));
+            record.Add($"The product of {int.Parse(firstNumber)} and {int.Parse(secondNumber)} is " + (int.Parse(firstNumber) * int.Parse(secondNumber)) + ".");
             break;
         case ("/"):
             Console.WriteLine("The quotient is " + (int.Parse(firstNumber) / int.Parse(secondNumber)));
-            record.Add("The sum is " + (int.Parse(firstNumber) / int.Parse(secondNumber)));
+            record.Add($"The quotient of {int.Parse(firstNumber)} and {int.Parse(secondNumber)} is " + (int.Parse(firstNumber) / int.Parse(secondNumber)) + ".");
             break;
         default:
             Console.WriteLine("Wrong input");
             break;
     }
+    startGame();
 }
 
-//startGame();
+void checkInput(string input)
+{
+    if (input == "help")
+    {
+        getHelp();
+    }
+    else if (input == "history")
+    {
+        foreach (string game in record)
+        {
+            Console.WriteLine(game);
+        }
+    }
+    else if (int.TryParse(input, out int n) == false)
+    {
+        wrongInput();
+    }
+}
+
+void wrongInput()
+{
+    Console.WriteLine("Your input is not a number!");
+    Console.WriteLine("The app has been restarted.");
+    startGame();
+}
+
+startGame();
 
 void getHelp()
 {
@@ -58,6 +75,7 @@ void getHelp()
     Console.WriteLine("A number is expected in 'First number'");
     Console.WriteLine("A number is expected in 'Second number'");
     Console.WriteLine("A arithmetic operator (+,-,*,/) is expected in 'Arithmetic operation'");
-    Console.WriteLine();
+    Console.WriteLine("Write 'help' to see this output.");
+    Console.WriteLine("write 'history' to see your previous calculations.");
     startGame();
 }
